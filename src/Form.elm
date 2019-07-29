579bb3cbe702876cb0e8ee051fd451cd3ba6fcdc
Jerome Bergmann <bergmann.jerome@gmail.com>
2019-07-29 07:51:24 +0200

    First Commit

---
+++ src/Form.elm
@@ -0,0 +1,138 @@
+module Form exposing (InputType(..), Model, Msg, decoder, update, view)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Json.Decode as Decode exposing (..)
+import Number.Bounded as Bounded exposing (..)
+
+
+type InputType
+    = InputString
+    | InputNumber (Maybe (Bounded Int))
+    | InputChoice
+
+
+type Msg
+    = Change Int String
+    | Message
+
+
+type alias Model =
+    { forms : List Form }
+
+
+type alias Form =
+    { label : String
+    , formtype : InputType
+    , content : String
+    }
+
+
+decoder =
+    Decode.list
+        (Decode.map3 Form
+            (Decode.field "label" Decode.string)
+            (Decode.field "formtype" inputTypeDecoder)
+            (Decode.succeed "")
+        )
+
+
+inputTypeDecoder =
+    Decode.string
+        |> Decode.andThen
+            (\str ->
+                case str of
+                    "String" ->
+                        Decode.succeed InputString
+
+                    "Number" ->
+                        Decode.succeed (InputNumber (Just (Bounded.between 1 10)))
+
+                    somethingElse ->
+                        Decode.fail <| "Unknown Type: " ++ somethingElse
+            )
+
+
+view : Model -> Html Msg
+view model =
+    List.indexedMap viewForm model.forms
+
+viewForm : Int -> Form -> Html Msg
+viewForm index form =
+    case form.formtype of
+        InputString ->
+            viewInputString index form
+
+        InputNumber n ->
+            viewInputNumber index form n
+
+        InputChoice ->
+            viewInputChoice index form
+
+
+viewInputString : Int -> Form -> Html Msg
+viewInputString index model =
+    Html.div
+        []
+        [ text model.label
+        , Html.input
+            [ Html.Attributes.placeholder "Your answer"
+            , Html.Attributes.value model.content
+            , Html.Events.onInput (Change index)
+            ]
+            []
+        , text model.content
+        ]
+
+
+viewInputNumber : Int -> Form -> Maybe (Bounded Int) -> Html Msg
+viewInputNumber index model bounds =
+    Html.div
+        []
+        [ text model.label
+        , Html.input
+            [ Html.Attributes.placeholder "Your answer"
+            , Html.Attributes.value model.content
+            , Html.Events.onInput (Change index)
+            ]
+            []
+        , text model.content
+        ]
+
+
+viewInputChoice : Int -> Form -> Html Msg
+viewInputChoice index model =
+    Html.div
+        []
+        [ text model.label
+        , Html.input
+            [ Html.Attributes.placeholder "Your answer"
+            , Html.Attributes.value model.content
+            , Html.Events.onInput (Change index)
+            ]
+            []
+        , text model.content
+        ]
+
+
+updateElement list id value =
+  let
+    alter idx form =
+      if id == idx then
+        { form | content = value}
+      else
+        form
+  in
+    List.indexedMap alter list
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Change index newContent ->
+            let forms_ = model.forms 
+            in 
+            ( { model | forms = forms_ }, Cmd.none )
+
+        Message ->
+            ( model, Cmd.none )
---
+++ src/Main.elm
@@ -0,0 +1,69 @@
+port module Main exposing (Msg(..), activeUsers)
+
+import Browser exposing (..)
+import Form exposing (..)
+import Html exposing (..)
+import Json.Decode as Decode exposing (..)
+import Json.Encode as E
+
+
+type Msg
+    = Searched String
+    | Changed E.Value
+    | FormMsg Form.Msg
+
+
+type alias Model =
+    { form : Form.Model
+    }
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+init : Value -> ( Model, Cmd msg )
+init flags =
+    case Decode.decodeValue Form.decoder flags of
+        Ok model ->
+            ( Model model, Cmd.none )
+
+        Err err ->
+            ( Model (Form.Model (Decode.errorToString err) Form.InputString "ERROR"), Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Searched str ->
+            ( model, Cmd.none )
+
+        Changed v ->
+            ( model, Cmd.none )
+
+        FormMsg fmsg ->
+            let (formModel, formCmd) = Form.update fmsg model.form
+            in
+            ( { model | form = formModel}, 
+            Cmd.map FormMsg formCmd )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    Html.div []
+        [ Html.map FormMsg (Form.view model.form)
+        , Html.map FormMsg (Form.view model.form)
+        ]
+
+
+port activeUsers : (E.Value -> msg) -> Sub msg
